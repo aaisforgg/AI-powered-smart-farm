91d517661ec981d35cdbe2b67a8cae399456c841
XavierSs21 <xavier.sotomayor21@hotmail.com>
2026-03-04 00:27:43 -0600

    fix agent - corregir  asignación de goal y strategy en update

--- agent/agent.py
+++ agent/agent.py
@@ -8,29 +8,27 @@ class Agent:
         self.y = y
 
         self.goal = None
+        self.strategy = None
 
         self.decision_system = DecisionSystem()
         self.movement = Movement()
 
     def update(self, grid):
 
-        # Si no tiene meta, pensar
         if not self.goal:
-            self.goal = self.decision_system.decide(grid, self)
+            self.goal, self.strategy = self.decision_system.decide(grid, self)
 
-        # Si tiene meta
         if self.goal:
 
             if (self.x, self.y) == self.goal:
                 print("Llegó al cultivo!")
                 self.goal = None
+                self.strategy = None
             else:
                 moved = self.movement.move_towards(self, grid, self.goal)
 
-                # Si no pudo moverse hacia meta, explorar
                 if not moved:
                     self.movement.random_move(self, grid)
 
         else:
-            # Si no hay meta, explorar
             self.movement.random_move(self, grid)
