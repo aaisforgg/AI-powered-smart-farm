483b16e298d3de3857dfc7bb26fabd27d3e2f000
XavierSs21 <xavier.sotomayor21@hotmail.com>
2026-03-14 18:15:48 -0600

    fix, corrige bugs del agente - movimiento, cosecha, crecimiento y decision

--- entities/crop.py
+++ entities/crop.py
@@ -1,12 +1,18 @@
 
 class Crop:
+
     def __init__(self, x, y):
         self.pos = (x, y)
         self.humedad = 100.0
         self.fase = 0
+        self.ticks_en_fase = 0
 
-    def crecer(self, tasa_secado, umbral_crecimiento):
+    def crecer(self, tasa_secado, ticks_por_fase=20):
         if self.humedad > 0:
             self.humedad -= tasa_secado
-            if self.humedad > umbral_crecimiento:
-                self.fase = min(self.fase + 1, 2)
+
+        if self.fase < 2:
+            self.ticks_en_fase += 1
+            if self.ticks_en_fase >= ticks_por_fase:
+                self.fase += 1
+                self.ticks_en_fase = 0
