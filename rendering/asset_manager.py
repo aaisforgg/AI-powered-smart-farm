d91d5598067f73329737c4aec3b4017efa323a77
derbyby <145092950+Derbyby@users.noreply.github.com>
2026-03-15 23:04:40 -0600

    Se agregaron cosas visuales

--- rendering/asset_manager.py
+++ rendering/asset_manager.py
@@ -112,7 +112,7 @@ class AssetManager:
                 self._maps[season] = surface
                 print(f"[Assets] Mapa '{season}' cargado desde '{loaded_from}'")
 
-        overlay = self._try_load_map("assets/map_overlay.png", target, alpha=False)
+        #overlay = self._try_load_map("assets/map_overlay.png", target, alpha=False)
         # if overlay is not None:
         #     overlay.set_alpha(160)   # semi-transparente: mapa estacional visible debajo
         #     self._map_overlay = overlay
